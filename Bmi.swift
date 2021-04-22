fe164aaea0696f5bad41d22351c0f20d84bf8ebd
ferdinandu-creator <72628587+ferdinandu-creator@users.noreply.github.com>
2021-04-22 17:56:46 +0000

    Add files via upload

---
+++ Bmi.swift
@@ -0,0 +1,15 @@
+//
+//  Bmi.swift
+//  BMI Calculator
+//
+//  Created by Fernando Oliveira on 04/04/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
